993fe0d4c7b49fcc4b503b87fe45f04abdbcf2a6
Taylor <taylorknoah@gmail.com>
2022-05-25 15:03:35 -0700

    add logger

--- microservices/Auth/lend_an_auth/src/config/mod.rs
+++ microservices/Auth/lend_an_auth/src/config/mod.rs
@@ -2,7 +2,8 @@ use color_eyre::Result;
 use eyre::WrapErr;
 use serde::Deserialize;
 use dotenv::dotenv;
-
+use tracing::{info, instrument};
+use tracing_subscriber::EnvFilter;
 
 
 
@@ -13,14 +14,22 @@ pub struct Config {
 }
 
 impl Config {
+
+	#[instrument]
 	pub fn from_env() -> Result<Config> {
 		dotenv().ok();
+
+		tracing_subscriber::fmt()
+			.with_env_filter(EnvFilter::from_default_env())
+			.init();
+
+		info!("Loading configuration.");
 
 		let mut cfg = config::Config::new();
 
 		cfg.merge(config::Environment::default())?;
 
 		cfg.try_into()
-			.context("LaAuth: loading configuration from environment")
+			.context("Loading configuration from environment")
 	}
 }
--- microservices/Auth/lend_an_auth/src/main.rs
+++ microservices/Auth/lend_an_auth/src/main.rs
@@ -1,7 +1,8 @@
 mod config;
 
 use color_eyre::Result;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
+use tracing::info;
 
 use crate::config::Config;
 
@@ -11,10 +12,13 @@ use crate::config::Config;
 async fn main() -> Result<()>{
     
 	let config = Config::from_env()
-		.expect("LaAuth: Server Configuration");
+		.expect("Server Configuration");
+	
+	info!("Starting server at http://{}:{}",config.host,config.port);
 
-	HttpServer::new(move ||{
+	HttpServer::new(move || {
 		App::new()
+			.wrap(Logger::default())
 	})
 	.bind(format!("{}:{}",config.host, config.port))?
 	.run()
